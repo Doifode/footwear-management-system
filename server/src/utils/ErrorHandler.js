95d11463a1494a86e2ba47c1ffc015a861b1f266
Doifode <yashdada9001@gmail.com>
2024-09-01 20:06:02 +0530

    changes to create crud operation api for shop and delete and restore api also. and store procedures also.

---
+++ server/src/utils/ErrorHandler.js
@@ -0,0 +1,9 @@
+// errorHandler.js
+export const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(err.statusCode || 500).json({
+        status: false,
+        message: err.message || 'Internal Server Error',
+    });
+};
