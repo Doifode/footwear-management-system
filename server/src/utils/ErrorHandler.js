5f0fcd1c9d1a1e4e249a846471681ae30440dcb0
Yash Doifode <115577913+Doifode@users.noreply.github.com>
2024-09-19 14:17:05 +0530

    Revert "Yash/api/auth middleware 18092024"

--- server/src/utils/ErrorHandler.js
+++
@@ -1,9 +0,0 @@
-// errorHandler.js
-export const errorHandler = (err, _req, res, _next) => {
-    console.error(err.stack);
-
-    res.status(err.statusCode || 200).json({
-        status: false,
-        message: err.sqlMessage || 'Internal Server Error',
-    });
-};
